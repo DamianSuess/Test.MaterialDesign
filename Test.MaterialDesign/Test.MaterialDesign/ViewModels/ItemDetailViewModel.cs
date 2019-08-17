4db3c6a46199e57485f89f2e918f5ddacec3a703
Damian Suess <dsuess@xenoinc.org>
2019-08-17 07:36:07 -0400

    init

---
+++ Test.MaterialDesign/Test.MaterialDesign/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Test.MaterialDesign.Models;
+
+namespace Test.MaterialDesign.ViewModels
+{
+  public class ItemDetailViewModel : BaseViewModel
+  {
+    public Item Item { get; set; }
+    public ItemDetailViewModel(Item item = null)
+    {
+      Title = item?.Text;
+      Item = item;
+    }
+  }
+}
